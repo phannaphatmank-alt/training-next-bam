419f6dbe7e52c98eea49ac323e2c5b00f26e3596
bam <phannaphatmankhong@bam-6.local>
2025-09-11 19:11:24 +0700

    first commit

---
+++ src/app/about-us/page.js
@@ -0,0 +1,12 @@
+import Navbar from "@/app/components/Navbar";
+import AboutUsSection from "@/app/components/AboutUsSection";
+
+export default function AboutUs(){
+    return(
+        <div>
+            <Navbar />
+            <AboutUsSection />
+            <h2 className="text-2xl"></h2>
+        </div>
+    );
+}
